78dbdffb71cb23379b9272b681b651c394e1d942
aaidalol <aaidenlolextras@gmail.com>
2026-08-09 17:25:16 -0700

    Create packs.js

---
+++ js/packs.js
@@ -0,0 +1,61 @@
+export const packs = [
+    {
+        id: 'mtf-trilogy',
+        name: 'The MTF Trilogy',
+        description: 'Description here',
+        points: 0,
+        levels: [
+            'mccthree',
+            'tims',
+            'fillerahhlevel',
+        ],
+    },
+
+    {
+        id: 'og-spam-trilogy',
+        name: 'The OG Spam Trilogy',
+        description: 'Description here',
+        points: 0,
+        levels: [
+            'idekwhattsis',
+            'wavespamdog',
+            'mikuspamchal',
+        ],
+    },
+
+    {
+        id: 'fallen-rulers',
+        name: 'The Fallen Rulers',
+        description: 'Description here',
+        points: 0,
+        levels: [
+            'uwuchal',
+            'stsbuffed',
+            'poopycologne',
+        ],
+    },
+
+    {
+        id: 'spouses-husbands-and-wives',
+        name: 'Spouses, Husbands And Wives',
+        description: 'Description here',
+        points: 0,
+        levels: [
+            'wavespamdog',
+            'mikuspamchal',
+            'lockintwin',
+            'aacropolis',
+        ],
+    },
+
+    {
+        id: 'silly-ahh-levels',
+        name: 'Silly Ahh Levels',
+        description: 'Description here',
+        points: 0,
+        levels: [
+            'sillyrobochal',
+            'uwuchal',
+        ],
+    },
+];
